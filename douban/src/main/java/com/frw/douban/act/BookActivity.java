407f9409087dadd1dc2b56671a63b43dedefe64d
fruwei <fruwei12@gmail.com>
2016-01-25 22:42:04 +0800

    jar

--- douban/src/main/java/com/frw/douban/act/BookActivity.java
+++ douban/src/main/java/com/frw/douban/act/BookActivity.java
@@ -3,6 +3,8 @@ package com.frw.douban.act;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.frw.douban.R;
+
 public class BookActivity extends AppCompatActivity {
 
     @Override

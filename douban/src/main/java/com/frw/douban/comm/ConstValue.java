527068061afad97bd17f1a9e5ecf8cba6d73157f
frubby <fruwei12@gmail.com>
2016-01-25 23:12:59 +0800

    create douban

---
+++ douban/src/main/java/com/frw/douban/comm/ConstValue.java
@@ -0,0 +1,12 @@
+package com.frw.douban.comm;
+
+/**
+ * Created by frw on 2016/1/25.
+ */
+public class ConstValue {
+    public static  final  String DB_BASE="http://api.douban.com/";
+    public  static final  String MOVIE_NOW="/v2/movie/in_theaters";
+
+    public  static final  String MOVIE_SOON="/v2/movie/coming_soon";
+    public  static final  String MOVIE_250="/v2/movie/top250";
+}

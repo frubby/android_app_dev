50e25fc2ca03504d77a7f4d465a22434cea0f22a
frubby <fruwei12@gmail.com>
2016-01-25 19:09:10 +0800

    add image loader

--- app/src/main/java/com/frw/frwapp/fragm/PersonFragment.java
+++ app/src/main/java/com/frw/frwapp/fragm/PersonFragment.java
@@ -68,8 +68,6 @@ public class PersonFragment extends Fragment implements WeiboActivity.OnFragMain
                 .memoryCacheExtraOptions(480, 800) // default = device screen dimensions
                 .diskCacheExtraOptions(480, 800, null)
                 .threadPoolSize(3) // default
-                .threadPriority(Thread.NORM_PRIORITY - 2) // default
-                .tasksProcessingOrder(QueueProcessingType.FIFO) // default
                 .denyCacheImageMultipleSizesInMemory()
                 .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
                 .memoryCacheSize(2 * 1024 * 1024)
